f1a0b0f52be72055b5ef979e19f08d0d6f0ad36e
Hammad Yousuf <hammadbhat126@gmail.com>
2021-12-23 07:01:22 +0400

    Store Date

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,28 @@
 class News{
 
-  
+  late String id;
+  late String url;
+  late String title;
+  late String text;
+  late String publisher;
+  late String author;
+  late String image;
+  late String date;
+
+
+  News(this.id, this.url, this.title, this.text, this.publisher,this.author, this.image, this.date);
+
+ News.fromJson(Map<String ,dynamic> json){
+    id = json['id'];
+    url = json['url'];
+    title = json['title'];
+    text = json['text'];
+    publisher = json['publisher'];
+    author = json['author'];
+    image = json['image'];
+    date = json['date'];
+    url = json['url'];
+
+  }
+
 }
